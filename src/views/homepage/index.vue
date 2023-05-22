4f9ad1a43835d19b776bd2df6fecfce0da378405
sghuangrihuang <huangrihuang@zdnst.gz>
2023-05-22 11:33:36 +0800

    feat(主页): 组件划分头部

--- src/views/homepage/index.vue
+++ src/views/homepage/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import HomePageHeader from "./components/header.vue";
 const routerActive = ref('/homepage/home')
 const roterList = ref([
   {
@@ -19,13 +20,7 @@ const roterList = ref([
 
 <template>
   <div class="z-homepage-container">
-    <div class="z-homepage-header">
-      <div class="z-homepage-logo"></div>
-      <el-menu class="z-homepage-menu" router :default-active="routerActive" :ellipsis="false" mode="horizontal">
-        <el-menu-item v-for="item in roterList" :index="item.path" :key="item.path">{{ item.title }}</el-menu-item>
-      </el-menu>
-      <div class="z-homepage-setting"></div>
-    </div>
+    <HomePageHeader class="z-homepage-header" />
     <div class="z-homepage-container">
       <router-view></router-view>
     </div>
@@ -40,22 +35,8 @@ const roterList = ref([
   flex-direction: column;
   overflow: hidden;
   .z-homepage-header {
-    flex: 0 0 auto;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+    position: relative;
     border-bottom: solid 1px var(--el-menu-border-color);
-    .z-homepage-logo {
-      flex: 0 0 50px;
-      width: 50px;
-    }
-    .z-homepage-menu {
-      border-bottom: none;
-    }
-    .z-homepage-setting {
-      flex: 0 0 50px;
-      width: 50px;
-    }
   }
   .z-homepage-container {
     flex: 1;
