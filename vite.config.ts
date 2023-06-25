f26604b1d603d558fa8ffafb53e7ac724251732f
sghuangrihuang <huangrihuang@zdnst.gz>
2023-06-25 10:56:58 +0800

    build: 新增自动引用import依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router'
+      ],
+      dts: './src/auto-import.d.ts'
+    }),
+  ],
   base: './',	//不加打包后白屏
   // server: {             
   //   host: '0.0.0.0',	
