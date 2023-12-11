6d048eb2815c1d904b456dc0fcb2fdce9d1a2a9d
sghuangrihuang <huangrihuang@zdnst.gz>
2023-12-11 16:15:22 +0800

    feat

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
+<script setup lang="ts">
+
+</script>
 <template>
   <router-view></router-view>
 </template>
-<script setup lang="ts">
-</script>
-<style>
-#app {
-  height: 100%;
-  color: var(--el-text-color-primary);
-  background: var(--el-bg-color);
-}
 
-.element-plus-logo {
-  width: 50%;
-}
+<style>
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
-import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
@@ -11,15 +11,13 @@ import "uno.css";
 
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import locale from 'element-plus/lib/locale/lang/zh-cn';
-import { createPinia } from 'pinia'
 
-axios.defaults.baseURL=import.meta.env.VITE_APP_BASE_URL as any
-axios.defaults.timeout = 180*1000
 const app = createApp(App)
 Object.keys(ElementPlusIconsVue).forEach(key => {
   Reflect.has(ElementPlusIconsVue, key)
   && app.component(key, ElementPlusIconsVue[key as keyof typeof ElementPlusIconsVue]);
 })
+
 app
   .use(ElementPlus, { locale })
   .use(createPinia())
