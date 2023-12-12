e602d004ea6ec43b35e8a92f6c746dcc41f4986a
sghuangrihuang <huangrihuang@zdnst.gz>
2023-12-12 14:35:21 +0800

    feat

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,13 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
-
+import VConsole from 'vconsole'
 import 'element-plus/dist/index.css'
 import "~/styles/index.scss";
 import "uno.css";
-
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import locale from 'element-plus/lib/locale/lang/zh-cn';
-
+new VConsole()
 const app = createApp(App)
 Object.keys(ElementPlusIconsVue).forEach(key => {
   Reflect.has(ElementPlusIconsVue, key)
